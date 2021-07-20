00b47b00ae9cb8f43b58c46e72283f007b8afaad
gudari <aartola1986@gmail.com>
2021-07-21 00:44:08 +0200

    change release api address

--- create_release.sh
+++ create_release.sh
@@ -6,18 +6,16 @@ GITHUB_REPO=$2
 VERSION=$3
 GITHUB_TOKEN=$4
 
-echo "Exporting token and enterprise api to enable github-release tool"
-GITHUB_API=https://api.github.com
 RELEASE_NAME="release-${VERSION}-arm64"
 
 
 release=$(curl -XPOST -H "Authorization:token $GITHUB_TOKEN" \
     --data "{\"tag_name\": \"$RELEASE_NAME\", \"target_commitish\": \"$RELEASE_NAME\", \"name\": \"$RELEASE_NAME\", \"draft\": false }" \
-    $GITHUB_API/repos/$GITHUB_ORGANIZATION/$GITHUB_REPO/releases)
+    https://api.github.com/repos/$GITHUB_ORGANIZATION/$GITHUB_REPO/releases)
 
 id=$(echo "$release" | sed -n -e 's/"id":\ \([0-9]\+\),/\1/p' | head -n 1 | sed 's/[[:blank:]]//g')
 
 curl -XPOST -H "Authorization:token $GITHUB_TOKEN" \
     -H "Content-Type:application/octet-stream" \
-    --upload-file hadoop-dist/target/hadoop-$VERSION.tar.gz $GITHUB_API/repos/$GITHUB_ORGANIZATION/$GITHUB_REPO/releases/$id/assets?name=hadoop-$VERSION.tar.gz
+    --upload-file hadoop-dist/target/hadoop-$VERSION.tar.gz https://uploads.github.com/repos/$GITHUB_ORGANIZATION/$GITHUB_REPO/releases/$id/assets?name=hadoop-$VERSION.tar.gz
     
